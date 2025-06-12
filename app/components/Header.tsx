07cb8b7ef7859f3d9766f15d5e7295a394955192
Woojin Choi <79125806+woojin-choi518@users.noreply.github.com>
2025-06-12 15:49:21 +0900

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -15,21 +15,6 @@ export default function Header() {
           <Link href="/tree" className="hover:underline">보호수</Link>
           <Link href="/prod" className='hover:underline'>농작물</Link>
         </div>
-
-        {/* 오른쪽: 로고와 텍스트 */}
-        <div className="flex items-center space-x-2 mt-2 sm:mt-0">
-          <div className="w-8 h-8 relative">
-            <Image
-              src="/images/sunmoonLogo.svg" // 실제 경로로 수정
-              alt="Sunmoon Logo"
-              fill
-              className="object-contain"
-            />
-          </div>
-          <span className="font-bold whitespace-nowrap text-sm sm:text-base">
-            선문대학교
-          </span>
-        </div>
       </nav>
     </header>
   )
