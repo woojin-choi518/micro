5af51d482cb763899fb3bd678b8331d95ffbb9ec
woojin-choi518 <woojin.choi518@gmail.com>
2025-09-21 12:14:35 +0900

    refactor: comment out feature sections in page.tsx for future updates

--- app/page.tsx
+++ app/page.tsx
@@ -14,41 +14,41 @@ export default function Page() {
 
       {/* Feature 섹션 (SSR) */}
       {[
-        {
-          title: 'Map · Sample Searching',
-          subTitle:'미생물 샘플, \n지도에서 간편하게',
-          description: 'Earth Microbiome Project의 60개 지역, 32개 환경에서 채집한 8,861개의 샘플을 편리하게 검색해보세요.',
-          image1: { src: '/images/search1.webp', alt: 'Search 화면 1' },
-          image2: { src: '/images/search2.webp', alt: 'Search 화면 2' },
-        },
-        {
-          title: 'Similarity · 샘플 간 유사도 분석',
-          subTitle:'유사도 분석, \n직관적인 GraphDB로! ',
-          description: 'Neo4j 그래프 DB로 관계 기반 분석을 지원합니다.',
-          image1: { src: '/images/graph1.webp', alt: 'Search 화면 1' },
-          image2: { src: '/images/graph2.webp', alt: 'Search 화면 2' },
-        },
-        {
-          title: 'Polar · 극지방 미생물 탐색',
-          subTitle:'극지방의 미생물, \n연도별 변화를 확인해보세요 ',
-          description: 'NCBI, PAMC에서 30여년간 수집한 40000개 이상의 데이터를 한눈에 확인해보세요. 연도별 미생물 변화와 좌표별 미생물 분포도 확인할 수 있습니다.',
-          image1: { src: '/images/polar1.webp', alt: 'Search 화면 1' },
-          image2: { src: '/images/polar2.webp', alt: 'Search 화면 2' },
-        },
-        {
-          title: 'Trees · 아산시 보호수 지도',
-          subTitle:'소중한 보호수, \n검색할 필요없이 \n한눈에 알아보세요! ',
-          description: '아산시의 약 800개의 보호수, 검색하시지 않아도 한눈에 보실 수 있어요. 필요하신 정보를 편하게 확인하세요.',
-          image1: { src: '/images/tree1.webp', alt: 'Search 화면 1' },
-          image2: { src: '/images/tree2.webp', alt: 'Search 화면 2' },
-        },
-        {
-          title: 'Crops · 곡물 생산량 분석',
-          subTitle:'작물 생산량과 미생물, \n둘 사이의 연관성을 확인해보세요.',
-          description: '미생물 다양성과 작물 생산성이 비례한다는 사실, 알고계셨나요? 실제 논문을 기반으로 한 데이터를 확인해보세요.',
-          image1: { src: '/images/crop1.webp', alt: 'Search 화면 1' },
-          image2: { src: '/images/crop2.webp', alt: 'Search 화면 2' },
-        },
+        // {
+        //   title: 'Map · Sample Searching',
+        //   subTitle:'미생물 샘플, \n지도에서 간편하게',
+        //   description: 'Earth Microbiome Project의 60개 지역, 32개 환경에서 채집한 8,861개의 샘플을 편리하게 검색해보세요.',
+        //   image1: { src: '/images/search1.webp', alt: 'Search 화면 1' },
+        //   image2: { src: '/images/search2.webp', alt: 'Search 화면 2' },
+        // },
+        // {
+        //   title: 'Similarity · 샘플 간 유사도 분석',
+        //   subTitle:'유사도 분석, \n직관적인 GraphDB로! ',
+        //   description: 'Neo4j 그래프 DB로 관계 기반 분석을 지원합니다.',
+        //   image1: { src: '/images/graph1.webp', alt: 'Search 화면 1' },
+        //   image2: { src: '/images/graph2.webp', alt: 'Search 화면 2' },
+        // },
+        // {
+        //   title: 'Polar · 극지방 미생물 탐색',
+        //   subTitle:'극지방의 미생물, \n연도별 변화를 확인해보세요 ',
+        //   description: 'NCBI, PAMC에서 30여년간 수집한 40000개 이상의 데이터를 한눈에 확인해보세요. 연도별 미생물 변화와 좌표별 미생물 분포도 확인할 수 있습니다.',
+        //   image1: { src: '/images/polar1.webp', alt: 'Search 화면 1' },
+        //   image2: { src: '/images/polar2.webp', alt: 'Search 화면 2' },
+        // },
+        // {
+        //   title: 'Trees · 아산시 보호수 지도',
+        //   subTitle:'소중한 보호수, \n검색할 필요없이 \n한눈에 알아보세요! ',
+        //   description: '아산시의 약 800개의 보호수, 검색하시지 않아도 한눈에 보실 수 있어요. 필요하신 정보를 편하게 확인하세요.',
+        //   image1: { src: '/images/tree1.webp', alt: 'Search 화면 1' },
+        //   image2: { src: '/images/tree2.webp', alt: 'Search 화면 2' },
+        // },
+        // {
+        //   title: 'Crops · 곡물 생산량 분석',
+        //   subTitle:'작물 생산량과 미생물, \n둘 사이의 연관성을 확인해보세요.',
+        //   description: '미생물 다양성과 작물 생산성이 비례한다는 사실, 알고계셨나요? 실제 논문을 기반으로 한 데이터를 확인해보세요.',
+        //   image1: { src: '/images/crop1.webp', alt: 'Search 화면 1' },
+        //   image2: { src: '/images/crop2.webp', alt: 'Search 화면 2' },
+        // },
         {
           title: 'Asan · 아산시 악취 지도',
           subTitle:'집 주변 악취 범위, \n날씨 기반으로 계산해서 알려드려요. ',
